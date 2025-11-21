65844396b3dfd900b4427a35dab762fd21755b9f
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-11-21 16:43:27 +0300

    refactor: extract reusable `x-home-about-us` component and integrate it into the homepage

---
+++ resources/views/components/home-about-us.blade.php
@@ -0,0 +1,58 @@
+@props([
+    /**@var\null*/"ourHistory",
+    /**@var\null*/"ourMission",
+    /**@var\null*/"ourVision",
+])
+
+<div
+    {{ $attributes->class(["container-fluid no-left-padding no-right-padding about-section d-flex flex-column align-items-center"]) }}
+    id="about-us-container"
+>
+    <div
+        class="about-img-block col-md-6 col-xs-12 no-left-padding no-right-padding"
+        data-image="assets/images/about-image.jpg"
+    ></div>
+    <!-- Container -->
+    <div class="container">
+        <div class="col-md-offset-6 col-md-6 col-xs-12 about-content">
+            <!-- Section Header -->
+            <div class="section-header">
+                <h3>ABOUT US</h3>
+            </div>
+            <!-- Section Header /- -->
+            <div class="about-details">
+                <div class="about-details-box">
+                    <i>
+                        <img
+                            src="{{ asset("assets/images/about-cnt-icon-1.png") }}"
+                            alt="Icon"
+                        />
+                    </i>
+                    <h4>Our History</h4>
+                    <p>{{ $ourHistory->content }}</p>
+                </div>
+                <div class="about-details-box">
+                    <i>
+                        <img
+                            src="{{ asset("assets/images/about-cnt-icon-2.png") }}"
+                            alt="Icon"
+                        />
+                    </i>
+                    <h4>Our Mission</h4>
+                    <p>{{ $ourMission->content }}</p>
+                </div>
+                <div class="about-details-box">
+                    <i>
+                        <img
+                            src="{{ asset("assets/images/about-cnt-icon-3.png") }}"
+                            alt="Icon"
+                        />
+                    </i>
+                    <h4>Our Vision</h4>
+                    <p>{{ $ourVision->content }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Container /- -->
+</div>
